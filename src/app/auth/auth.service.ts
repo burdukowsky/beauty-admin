6cf150c7bb711246dec20918364affab57cf1418
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2018-03-11 14:24:28 +0700

    improve login form

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,9 +3,6 @@ import {JwtHelperService} from '@auth0/angular-jwt';
 import {User} from './user';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import {catchError} from 'rxjs/operators';
-import {Observable} from 'rxjs/Observable';
-import {of} from 'rxjs/observable/of';
 import {Router} from '@angular/router';
 import {globals} from '../globals';
 
@@ -13,13 +10,6 @@ import {globals} from '../globals';
 export class AuthService {
 
   constructor(private jwtHelperService: JwtHelperService, private http: HttpClient, private router: Router) {
-  }
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(`${operation} failed: ${error.message}`);
-      return of(result as T);
-    };
   }
 
   loggedIn() {
@@ -32,10 +22,7 @@ export class AuthService {
   }
 
   login(user: User) {
-    return this.http.post(environment.apiEndpoint + '/login', user, {responseType: 'text', observe: 'response'})
-      .pipe(
-        catchError(this.handleError('login', []))
-      );
+    return this.http.post(environment.apiEndpoint + '/login', user, {responseType: 'text', observe: 'response'});
   }
 
   logout() {
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,8 @@ import {HttpResponse} from '@angular/common/http';
 import {Router, ActivatedRoute} from '@angular/router';
 import {globals} from '../globals';
 import {TranslateService} from '@ngx-translate/core';
+import {Subject} from 'rxjs/Subject';
+import {debounceTime} from 'rxjs/operator/debounceTime';
 
 @Component({
   selector: 'app-login',
@@ -15,6 +17,8 @@ export class LoginComponent implements OnInit {
   returnUrl: string;
   form: FormGroup;
   languagesClasses = {};
+  errorMessage: boolean;
+  private _error = new Subject<boolean>();
 
   constructor(private fb: FormBuilder,
               private auth: AuthService,
@@ -26,6 +30,9 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+    this._error.subscribe((state) => this.errorMessage = state);
+    debounceTime.call(this._error, globals.alertTimeout).subscribe(() => this.errorMessage = false);
+
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
 
     if (this.auth.loggedIn()) {
@@ -47,7 +54,10 @@ export class LoginComponent implements OnInit {
             localStorage.setItem(globals.localStorageKeys.accessToken, authorizationHeader.split(' ')[1]);
             this.router.navigateByUrl(this.returnUrl);
           },
-          err => console.log(err)
+          error => {
+            this._error.next(true);
+            console.error(error);
+          }
         );
     }
   }
